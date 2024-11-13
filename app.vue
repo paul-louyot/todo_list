431e0d8d1221b6fe201cc7363b90fa3eb40b5954
Paul Louyot <paul@Pauls-MacBook-Pro.local>
2024-11-13 11:52:08 +0100

    First commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="h-screen w-screen">
+    <TodoList/>
+    <!-- <NuxtRouteAnnouncer />
+    <NuxtWelcome /> -->
+  </div>
+</template>
---
+++ components/TodoList.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {XMarkIcon, ArrowDownIcon} from '@heroicons/vue/24/outline';
+</script>
+
+<template>
+  <div class="max-w-2xl mx-auto p-4 space-y-4">
+  <div class="card shadow">
+    <div class="card-body">
+      <textarea class="textarea textarea-bordered" placeholder="Bio"></textarea>
+      <button class="btn btn-primary">Create tasks</button>
+    </div>
+  </div>
+  <div class="card shadow">
+    <div class="card-body">
+      <div class="flex items-center gap-4 p-4 border rounded-lg bg-base-100">
+        <div class="flex-1">Task content</div>
+        <ArrowDownIcon class="size-5"/>
+        <XMarkIcon class="size-6"/>
+        <input type="checkbox" class="checkbox" />
+
+      </div>
+    </div>
+  </div>
+</div>
+
+</template>
