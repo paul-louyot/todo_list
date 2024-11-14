b8e6b73aa55cedc36f9b6be796a8381c7139418b
Paul Louyot <paul@device-5443.home>
2024-11-14 18:56:05 +0100

    Add background

--- app.vue
+++ app.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="h-screen w-screen relative">
     <div
-      class="absolute inset-0 z-0"
-      style="background-image: repeating-linear-gradient(45deg, transparent, transparent 10px, rgb(248 250 252) 10px, rgb(248 250 252) 12px)"
-    ></div>
+      class="absolute inset-0 z-0 text-base-200"
+    >
+      <TheBackground />
+
+
+    </div>
 
     <div class="page-content z-10 relative h-full overflow-scroll">
       <div class="max-w-2xl mx-auto p-8 space-y-4">
